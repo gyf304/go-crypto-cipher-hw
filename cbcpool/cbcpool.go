71dc60a07723835a10044949a513adf4625ba15d
Yifan Gu <gyf304@users.noreply.github.com>
2023-03-01 17:46:26 +0000

    add cbcpool

---
+++ cbcpool/cbcpool.go
@@ -0,0 +1,83 @@
+package cbcpool
+
+import (
+	"crypto/cipher"
+	"errors"
+)
+
+type MutableBlockMode interface {
+	cipher.BlockMode
+	SetIV([]byte)
+	SetKey([]byte)
+}
+
+type Pool struct {
+	pool      chan MutableBlockMode
+	blockSize int
+}
+
+type pooledBlockMode struct {
+	pool *Pool
+	iv   []byte
+	key  []byte
+}
+
+func (p *pooledBlockMode) SetIV(iv []byte) {
+	if len(iv) != len(p.iv) {
+		panic("wrong IV length")
+	}
+	copy(p.iv, iv)
+}
+
+func (p *pooledBlockMode) SetKey(key []byte) {
+	if len(key) != len(p.key) {
+		panic("wrong key length")
+	}
+	copy(p.key, key)
+}
+
+func (p *pooledBlockMode) CryptBlocks(dst, src []byte) {
+	b := <-p.pool.pool
+	b.SetIV(p.iv)
+	b.CryptBlocks(dst, src)
+	srcLen := len(src)
+	newIV := dst[srcLen-p.pool.blockSize : srcLen]
+	p.SetIV(newIV)
+	p.pool.pool <- b
+}
+
+func (p *pooledBlockMode) BlockSize() int {
+	return p.pool.blockSize
+}
+
+func NewPool(blockSize int, modes ...cipher.BlockMode) (*Pool, error) {
+	if blockSize <= 0 {
+		return nil, errors.New("blockSize must be positive")
+	}
+	pool := &Pool{
+		pool:      make(chan MutableBlockMode, len(modes)),
+		blockSize: blockSize,
+	}
+	for _, mode := range modes {
+		ivMode, ok := mode.(MutableBlockMode)
+		if !ok {
+			return nil, errors.New("mode does not support SetIV")
+		}
+		if ivMode.BlockSize() != blockSize {
+			return nil, errors.New("mode has wrong block size")
+		}
+		pool.pool <- ivMode
+	}
+	return pool, nil
+}
+
+func (p *Pool) Get(key []byte, iv []byte) cipher.BlockMode {
+	m := &pooledBlockMode{
+		pool: p,
+		iv:   make([]byte, p.blockSize),
+		key:  make([]byte, len(key)),
+	}
+	m.SetIV(iv)
+	m.SetKey(key)
+	return m
+}
